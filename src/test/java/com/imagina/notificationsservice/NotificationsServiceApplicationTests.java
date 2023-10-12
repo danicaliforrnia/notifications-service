d436722db4cff4db5f3195e4d51536a2d758fabd
stefanellid <stefanellid@media-saturn.com>
2023-10-12 18:41:09 +0200

    first commit

---
+++ src/test/java/com/imagina/notificationsservice/NotificationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagina.notificationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
